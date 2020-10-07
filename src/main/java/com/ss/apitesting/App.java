795d57e6b22eb843d282f71b4cd6a79f343d6714
Boris Yevstyhneiev <molnerats@gmail.com>
2020-10-07 22:17:44 +0300

    renaming package from org.example to com.ss.apitesting

--- src/main/java/com/ss/apitesting/App.java
+++ src/main/java/com/ss/apitesting/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.ss.apitesting;
 
 /**
  * Hello world!
--- src/test/java/com/ss/apitesting/AppTest.java
+++ src/test/java/com/ss/apitesting/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.ss.apitesting;
 
 import static org.junit.Assert.assertTrue;
 
