f071de4ae9105c31d8c0774d392bfd532526882b
Udzhas <ura_dzhas@ukr.net>
2020-10-22 00:08:13 +0300

    fix errors and add update option

--- src/test/java/com/ss/apitesting/AddPetTest.java
+++ src/test/java/com/ss/apitesting/AddPetTest.java
@@ -36,69 +36,69 @@ public class AddPetTest {
     }
 
 
-//    @Test
-//    public void testAddValidPet() {
-//        // Precondition
-//        int freeId = findFreeID();
-//
-//        // Adding pet
-//        petClient.createPet(JSONDataProvider.getPetInJSON("" + freeId, "doggo", "available"));
-//        int creatingStatus = petClient.getById("" + freeId).getStatusCode();
-//        Assert.assertEquals(creatingStatus, 200);
-//
-//        // Remove added pet
-//        petClient.deleteById("" + freeId);
-//    }
-//
-//    @Test
-//    public void testAddUnnamedPet() {
-//        // Precondition
-//        int freeId = findFreeID();
-//
-//        // Adding pet
-//        petClient.createPet(JSONDataProvider.getPetInJSON("" + freeId, "", "available"));
-//        int creatingStatus = petClient.getById("" + freeId).getStatusCode();
-//        Assert.assertEquals(creatingStatus, 200);
-//
-//        // Remove added pet
-//        petClient.deleteById("" + freeId);
-//    }
-//
-//    // Why we can add pet with existing ID?
-//    @Test
-//    public void testAddPetWithExistingId() {
-//        // Precondition
-//        int occupiedId = findOccupiedID();
-//
-//        // Adding pet
-//        int creatingStatus = petClient.createPet
-//                (JSONDataProvider.getPetInJSON("" + occupiedId, "temp", "available"))
-//                .statusCode();
-//        Assert.assertEquals(creatingStatus, 200);
-//
-//        // Remove added pet
-//        petClient.deleteById("" + occupiedId);
-//    }
-//
-//    @Test
-//    public void testAddPetWithInvalidId() {
-//        // Precondition
-//        String invalidId = "188888888888888888888888888";
-//
-//        // Adding pet
-//        int addingResult = petClient.createPet(JSONDataProvider.getPetInJSON(invalidId, "dog", "sold"))
-//                .statusCode();
-//        Assert.assertEquals(addingResult, 500);
-//    }
-//
-//    @Test
-//    public void testAddPetWithNonDigitId() {
-//        // Precondition
-//        String nonDigitId = "\"eee\"";
-//
-//        // Adding pet
-//        int addingResult = petClient.createPet(JSONDataProvider.getPetInJSON(nonDigitId, "dog", "available"))
-//                .statusCode();
-//        Assert.assertEquals(addingResult, 500);
-//    }
+    @Test
+    public void testAddValidPet() {
+        // Precondition
+        int freeId = findFreeID();
+
+        // Adding pet
+        petClient.createPet(JSONDataProvider.getPetInJSON("" + freeId, "doggo", "available"));
+        int creatingStatus = petClient.getById("" + freeId).getStatusCode();
+        Assert.assertEquals(creatingStatus, 200);
+
+        // Remove added pet
+        petClient.deleteById("" + freeId);
+    }
+
+    @Test
+    public void testAddUnnamedPet() {
+        // Precondition
+        int freeId = findFreeID();
+
+        // Adding pet
+        petClient.createPet(JSONDataProvider.getPetInJSON("" + freeId, "", "available"));
+        int creatingStatus = petClient.getById("" + freeId).getStatusCode();
+        Assert.assertEquals(creatingStatus, 200);
+
+        // Remove added pet
+        petClient.deleteById("" + freeId);
+    }
+
+    // Why we can add pet with existing ID?
+    @Test
+    public void testAddPetWithExistingId() {
+        // Precondition
+        int occupiedId = findOccupiedID();
+
+        // Adding pet
+        int creatingStatus = petClient.createPet
+                (JSONDataProvider.getPetInJSON("" + occupiedId, "temp", "available"))
+                .statusCode();
+        Assert.assertEquals(creatingStatus, 200);
+
+        // Remove added pet
+        petClient.deleteById("" + occupiedId);
+    }
+
+    @Test
+    public void testAddPetWithInvalidId() {
+        // Precondition
+        String invalidId = "188888888888888888888888888";
+
+        // Adding pet
+        int addingResult = petClient.createPet(JSONDataProvider.getPetInJSON(invalidId, "dog", "sold"))
+                .statusCode();
+        Assert.assertEquals(addingResult, 500);
+    }
+
+    @Test
+    public void testAddPetWithNonDigitId() {
+        // Precondition
+        String nonDigitId = "\"eee\"";
+
+        // Adding pet
+        int addingResult = petClient.createPet(JSONDataProvider.getPetInJSON(nonDigitId, "dog", "available"))
+                .statusCode();
+        Assert.assertEquals(addingResult, 500);
+    }
 }
