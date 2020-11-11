651d0ffdc069a94b4269975cb4de96a94384bddb
Boris Yevstyhneiev <molnerats@gmail.com>
2020-11-11 22:57:00 +0200

    Logging each test class into separate files

--- src/test/java/com/ss/apitesting/BaseTest.java
+++ src/test/java/com/ss/apitesting/BaseTest.java
@@ -3,6 +3,8 @@ package com.ss.apitesting;
 import com.ss.apitesting.util.TestNgListeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 
@@ -20,5 +22,12 @@ public abstract class BaseTest {
     @BeforeClass
     public void beforeClass() {
         initLogger();
+        MDC.put("testname", this.getClass().getSimpleName());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        initLogger();
+        MDC.remove("testname");
     }
 }
