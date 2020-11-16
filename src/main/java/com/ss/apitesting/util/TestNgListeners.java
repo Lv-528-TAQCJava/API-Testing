c774bd0407917dffbc12053f6a0a6414d4b32b0a
Boris Yevstyhneiev <molnerats@gmail.com>
2020-11-16 17:26:58 +0200

    Attaching logs to Allure report on test failure

--- src/main/java/com/ss/apitesting/util/TestNgListeners.java
+++ src/main/java/com/ss/apitesting/util/TestNgListeners.java
@@ -1,9 +1,15 @@
 package com.ss.apitesting.util;
 
+import io.qameta.allure.Attachment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestNgListeners implements ITestListener {
 
@@ -12,7 +18,7 @@ public class TestNgListeners implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         log.warn("The name of the testcase failed is: {}", result.getName());
-        //TODO add logs to Allure report
+        attachLogFile(result.getInstance().getClass().getSimpleName());
     }
 
     @Override
@@ -22,7 +28,7 @@ public class TestNgListeners implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult result) {
-        log.info("{} test case started", result.getName());
+        log.info("{}.{} test case started", result.getInstance().getClass().getSimpleName(), result.getName());
         //TODO ITextContext - MDC.put("testname", .getClass().getSimpleName());
         // + onFinish() MDC.remove("testname");
         // + logs from REST-assured
@@ -31,5 +37,17 @@ public class TestNgListeners implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         log.info("The name of the testcase passed is: {}", result.getName());
+    }
+
+    @Attachment(value = "Logs for {className}", type = "text/plain", fileExtension = ".log")
+    public byte[] attachLogFile(String className) {
+        try {
+            Path path = Paths.get(System.getProperty("user.dir") + "\\target\\logs\\"
+                    + className + ".log");
+            return Files.readAllBytes(path);
+        } catch (IOException ignored) {
+            log.warn("Logs for " + className + " are unavailable");
+        }
+        return null;
     }
 }
