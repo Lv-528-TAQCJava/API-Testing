177bf460a402e8017dc3a785fee877400f348003
Boris Yevstyhneiev <molnerats@gmail.com>
2020-11-25 21:19:14 +0200

    added rest-assured filters

--- src/test/java/com/ss/apitesting/BaseTest.java
+++ src/test/java/com/ss/apitesting/BaseTest.java
@@ -1,13 +1,18 @@
 package com.ss.apitesting;
 
-import com.ss.apitesting.util.TestNgAnnotationTransformer;
-import com.ss.apitesting.util.TestNgListeners;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 
 public abstract class BaseTest {
@@ -22,9 +27,27 @@ public abstract class BaseTest {
     @BeforeClass
     public void beforeClass() {
         initLogger();
-        MDC.put("testname", this.getClass().getSimpleName());
+        String className = this.getClass().getSimpleName();
+        MDC.put("testname", className);
         /* ^ Allows logging each test class into separate file.
         The <key> name is defined at logback-test.xml under <discriminator> tag*/
+        File logFile = new File(System.getProperty("user.dir") + "\\target\\logs\\"
+                + className + "_requests.log");
+        try {
+            logFile.createNewFile();
+            PrintStream stream = new PrintStream(logFile);
+            RestAssured.filters(
+                    new ResponseLoggingFilter(LogDetail.ALL, stream),
+                    new RequestLoggingFilter(LogDetail.ALL, stream)
+            );
+        } catch (IOException er) {
+            log.warn("Can not set RestAssured filters' output to file, will log to console.");
+            er.printStackTrace();
+            RestAssured.filters(
+                    new ResponseLoggingFilter(LogDetail.ALL),
+                    new RequestLoggingFilter(LogDetail.ALL)
+            );
+        }
     }
 
     @AfterClass
