11c1c0d790f54a632395bb5e70baf3f1dd02f11c
Boris Yevstyhneiev <molnerats@gmail.com>
2020-11-25 22:02:18 +0200

    adding rest-assured filters to allure report

--- src/main/java/com/ss/apitesting/util/TestNgListeners.java
+++ src/main/java/com/ss/apitesting/util/TestNgListeners.java
@@ -24,27 +24,27 @@ public class TestNgListeners implements ITestListener {
     public void onTestFailure(ITestResult result) {
         log.warn("The name of the testcase failed is: {}", result.getName());
         attachLogFile(result.getInstance().getClass().getSimpleName());
+        attachRequestLogFile(result.getInstance().getClass().getSimpleName());
         //TODO use retry analyzer (do something with the result param)
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
         log.info("The name of the testcase Skipped is: {}", result.getName());
+        attachLogFile(result.getInstance().getClass().getSimpleName());
     }
 
     @Override
     public void onTestStart(ITestResult result) {
-        log.info("{}.{} test case started", result.getInstance().getClass().getSimpleName(), result.getName());
-        //TODO ITextContext - MDC.put("testname", .getClass().getSimpleName());
-        // + onFinish() MDC.remove("testname");
-        // + logs from REST-assured
+        log.info("{}.{} test case started",
+                result.getInstance().getClass().getSimpleName(), result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         log.info("The name of the testcase passed is: {}", result.getName());
     }
-
+//---------------------------------------------------------------------------------
     @Attachment(value = "Logs for {className}", type = "text/plain", fileExtension = ".log")
     public byte[] attachLogFile(String className) {
         try {
@@ -53,6 +53,18 @@ public class TestNgListeners implements ITestListener {
             return Files.readAllBytes(path);
         } catch (IOException ignored) {
             log.warn("Logs for " + className + " are unavailable");
+        }
+        return null;
+    }
+
+    @Attachment(value = "Request logs for {className}", type = "text/plain", fileExtension = ".log")
+    public byte[] attachRequestLogFile(String className) {
+        try {
+            Path path = Paths.get(System.getProperty("user.dir") + "\\target\\logs\\"
+                    + className + "_requests.log");
+            return Files.readAllBytes(path);
+        } catch (IOException ignored) {
+            log.warn("Request logs for " + className + " are unavailable");
         }
         return null;
     }
